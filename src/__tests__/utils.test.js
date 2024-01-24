7f1c77e4740edcc3d7d41f9116ec03992f3ed813
JakobJohnson <madjzj@gmail.com>
2024-01-24 14:36:02 -0800

    Completed Lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,49 @@
-// Your tests here
+import {pointsForWord} from "../utils";
+
+describe("pointsForWord", ()=>{
+    it("calculates the total points for a word (1 point per vowel, 2 per consonant)", ()=>{
+        const word = "test";
+        const points= pointsForWord(word);
+        expect(points).toBe(7);
+    })
+})
+
+describe("pointsForWord", ()=>{
+    it("handles upercase letters", ()=>{
+        const word = "tESt";
+        const points= pointsForWord(word);
+        expect(points).toBe(7);
+    })
+})
+
+describe("pointsForWord", ()=>{
+    it("handles blank spaces", ()=>{
+        const word = "t e s t";
+        const points= pointsForWord(word);
+        expect(points).toBe(7);
+    })
+})
+
+describe("pointsForWord", ()=>{
+    it("handles non letter characters", ()=>{
+        const word = "t3est!";
+        const points= pointsForWord(word);
+        expect(points).toBe(7);
+    })
+})
+
+describe("pointsForWord", ()=>{
+    it("handles a blank string", ()=>{
+        const word = "";
+        const points= pointsForWord(word);
+        expect(points).toBe(0);
+    })
+})
+
+describe("pointsForWord", ()=>{
+    it("handles non string variable", ()=>{
+        const word = 52;
+        const points= pointsForWord(word);
+        expect(points).toBe(0);
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
-// Your code here
+export function pointsForWord(word) {
+    let points = 0;
+    if(typeof word != "string"){
+        return points
+    }
+    for (const char of word) {
+        if(/[a-z]/i.test(char)){
+            
+            points += /[aeiou]/i.test(char) ? 1 : 2;
+        }
+    }
+    return points;
+  }
